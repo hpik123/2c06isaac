cdbad888cf9c2666f1c0ca3441df86d155083b06
hpik123 <115450497+hpik123@users.noreply.github.com>
2022-10-10 14:59:45 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    basic.showNumber(0)
+    basic.showNumber(1)
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showNumber(4)
+    basic.showNumber(5)
+    basic.showNumber(6)
+})
